95efbaad12b739ceb7d2535bfa0002e423bc806b
Adam <avvallky@gmail.com>
2018-04-11 10:33:07 -0400

    Update do_this_before_running_server.sh

--- AppIntegration/do_this_before_running_server.sh
+++ AppIntegration/do_this_before_running_server.sh
@@ -1,5 +1,6 @@
-cd ./models
+# from object_detection/models
 protoc object_detection/protos/*.proto --python_out=.
 export PYTHONPATH=$PYTHONPATH:`pwd`:`pwd`/slim
 python object_detection/builders/model_builder_test.py
+cd ../
 python deploy_server.py
