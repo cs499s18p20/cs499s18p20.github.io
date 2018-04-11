4ab3084e5568355a302733c157ee702c6f2a665d
Adam <avvallky@gmail.com>
2018-04-11 11:36:01 -0400

    Update deploy_server.py

--- AppIntegration/deploy_server.py
+++ AppIntegration/deploy_server.py
@@ -14,8 +14,10 @@ from collections import defaultdict
 from matplotlib import pyplot as plt
 from PIL import Image
 sys.path.append("..")
-from object_detection.utils import label_map_util
-from object_detection.utils import visualization_utils as vis_util
+from utils import label_map_util
+from utils import visualization_utils as vis_util
+#from object_detection.utils import label_map_util
+#from object_detection.utils import visualization_utils as vis_util
 from threading import Thread
 from time import sleep
 ####################################################################################
